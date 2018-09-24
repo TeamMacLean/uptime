ca1408af4e5f283a4fcd59cdb519ed025a1164a5
Martin Page <wookoouk@gmail.com>
2018-09-24 14:15:03 +0100

    check for bad images

--- public/js/src/main.js
+++ public/js/src/main.js
@@ -1,3 +1,19 @@
 // import tippy from 'tippy.js'
 //
 // tippy('.btn');//https://atomiks.github.io/tippyjs/
+
+
+fixBrokenImages = function (url) {
+    var img = document.getElementsByTagName('img');
+    var i = 0, l = img.length;
+    for (; i < l; i++) {
+        var t = img[i];
+        if (t.naturalWidth === 0) {
+            //this image is broken
+            t.src = '/img/potato.png';
+        }
+    }
+}
+window.onload = function () {
+    fixBrokenImages('example.com/image.png');
+}
