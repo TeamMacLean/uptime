087923dd1070c5eb496b0ccada368e4089e612ab
Martin Page <wookoouk@gmail.com>
2018-11-08 16:18:13 +0000

    debug

--- public/js/src/main.js
+++ public/js/src/main.js
@@ -130,24 +130,47 @@ window.buildGraph = function (name, responses) {
     }
 
     const data = responses.reduce((all, r) => {
+
         all.labels.push(moment(r.createdAt).calendar());
         all.datasets.push(r.responseTime.toFixed(2));
         all.colors.push(getApdexColor(r));
         return all;
     }, {labels: [], datasets: [], colors: []});
 
-    console.log(data);
-
     const processedData = {
         labels: data.labels,
+        // labels: responses.map(function (r) {
+        //     return moment(r.createdAt).calendar();
+        // }),
         datasets: [{
             label: 'ms',
-            data: data.datasets,
+            data:data.datasets,
+            // data: responses.map(function (r) {
+            //     return r.responseTime.toFixed(2);
+            // }),
             fill: false,//'start',
-            backgroundColor: data.colors,
-            borderColor: data.colors,
+            // backgroundColor: responses.map(function (r) {
+            //     return getApdexColor(r);
+            // }),
+            backgroundColor:data.colors,
+            // // backgroundColor: [
+            // //     '#64EDC6'
+            // // ],
+            // borderColor: responses.map(function (r) {
+            //     return r.up ? '#64EDC6' : '#ff7675';
+            // }),
+            // borderColor: [
+            //     '#a29bfe'//'#64EDC6'
+            // ],
+            borderColor:data.colors,
+            // borderColor: responses.map(function (r) {
+            //     return getApdexColor(r);
+            // }),
+            // borderWidth: 1
         }]
     };
+
+    console.log(processedData);
 
     if (window.charts[name]) {
         window.charts[name].data = processedData;
