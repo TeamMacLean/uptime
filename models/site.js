d59d29a2c5091d732b1f4cf76e4e3182fb228b6d
Martin Page <wookoouk@gmail.com>
2019-02-07 09:55:54 +0000

    oops

--- models/site.js
+++ models/site.js
@@ -45,7 +45,7 @@ Site.pre('save', function (next) {
 
 Site.defineStatic('cleanup', function () {
     const month = 60 * 60 * 24 * 32; //32 days
-    return this.filter(token => token('expires_on').lt(thinky.r.now().sub(month))).delete().execute();
+    return this.filter(token => token('createdAt').lt(thinky.r.now().sub(month))).delete().execute();
 });
 
 Site.define('updateStats', function () {
